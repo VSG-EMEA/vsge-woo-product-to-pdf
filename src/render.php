c18186deb91f33648acc35aaa6eb9aaad10cadc3
Erik Golinelli <erik@codekraft.it>
2026-02-27 14:44:12 +0100

    enhances product PDF button rendering, adds post type data, improves attribute sanitization

--- src/render.php
+++ src/render.php
@@ -1,13 +1,16 @@
 <?php
-
+// src/render.php
 global $post;
+$post_type = get_post_type($post);
 ?>
 
 <a
-    href="javascript:"
-    title="<?php echo $attributes['title']; ?>"
-    data-product-id="<?php echo $post->ID; ?>"
+    href="javascript:void(0);"
+    class="wp-block-vsge-save-pdf-button"
+    title="<?php echo esc_attr($attributes['title'] ?? 'Download PDF'); ?>"
+    data-post-id="<?php echo esc_attr($post->ID); ?>"
+    data-post-type="<?php echo esc_attr($post_type); ?>"
     <?php echo get_block_wrapper_attributes(); ?>
 >
-    <?php echo $attributes['icon'] ?>
+    <?php echo wp_kses_post($attributes['icon'] ?? 'PDF'); ?>
 </a>
